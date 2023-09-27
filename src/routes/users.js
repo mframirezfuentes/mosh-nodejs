c91a4f31fe90aae53b3f95145fd573f83a0de8f8
Fernanda Ramírez <mframirezfuentes@gmail.com>
2023-09-26 21:47:25 -0300

    Hash a user password

--- src/routes/users.js
+++ src/routes/users.js
@@ -1,3 +1,4 @@
+const bcrypt = require("bcrypt");
 const _ = require("lodash");
 const express = require("express");
 const router = express.Router();
@@ -21,15 +22,12 @@ router.post("/", async (req, res) => {
   let user = await User.findOne({ email: req.body.email });
   if (user) return res.status(400).send("User already register");
 
-  user = new User({
-    name: req.body.name,
-    email: req.body.email,
-    password: req.body.password,
-  });
+  user = new User(_.pick(req.body, ["name", "email", "password"]));
+  const salt = await bcrypt.genSalt(10);
+  user.password = await bcrypt.hash(user.password, salt);
 
-
-  user = await user.save();
-  res.status(201).send( _.pick(user, ["_id","name", "email"]));
+  await user.save();
+  res.status(201).send(_.pick(user, ["_id", "name", "email"]));
 });
 
 router.put("/:email", async (req, res) => {
